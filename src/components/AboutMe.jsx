1e699e364ccc951c1030334a694b212ce96a7df9
mendana <diegodiazmenda@gmail.com>
2026-06-14 23:11:16 +0200

    feat(front): Actualiza info y añade segundo CV

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,39 +1,47 @@
 function AboutMe() {
+  return (
+    <div className="about-me-container">
+      <article>
+        <h2 className="text-5xl font-bold mb-8 lg:text-6xl text-[var(--text-primary)]">
+          ABOUT
+        </h2>
+        <p className="main-text mb-8">
+          I'm a young software developer with a strong interest in web
+          development, artificial intelligence, and emerging technologies. My
+          journey started with a deep curiosity for how web applications work
+          under the hood.
+        </p>
 
-    return (
-        <div className="about-me-container">
-            <article>
-                <h2 className='text-5xl font-bold mb-8 lg:text-6xl text-[var(--text-primary)]'>ABOUT</h2>
-                <p className='main-text mb-8'>
-                    I'm a self-taught developer passionate about creating digital experiences that blend functionality, design, and advanced technology. My journey started with a deep curiosity for how web applications work under the hood.
-                </p>
+        <p className="main-text mb-8">
+          Over time, I've focused on full-stack development and explored how
+          artificial intelligence can enhance user interaction and solve real
+          problems.
+        </p>
 
-                <p className='main-text mb-8'>
-                    Over time, I've focused on front-end development with React and explored how artificial intelligence can enhance user interaction and solve real problems.
-                </p>
-
-                <p className='main-text mb-8'>
-                    I'm currently focused on growing as a full-stack developer while experimenting with AI models, automation, and emerging technologies. I would like to start contributing to open-source projects while sharing what I've learn and staying curious.
-                </p>
-
-            </article>
-            <article>
-                <ul className="list-about">
-                    <li className="about-item">SERVICES</li>
-                    <ul className="list-about-item">
-                        <li>Frontend Development</li>
-                        <li>Backend Development</li>
-                        <li>Basic Data Analysis</li>
-                    </ul>
-                    <li className="about-item">RECOGNITION</li>
-                    <ul className="list-about-item">
-                        <li>60+ Platzi courses completed</li>
-                        <li>Studying maths and software at Oviedo</li>
-                    </ul>
-                </ul>
-            </article>
-        </div>
-    );
+        <p className="main-text mb-8">
+          I'm currently focused on growing as a full-stack developer while
+          experimenting with AI models, automation, and emerging technologies.
+        </p>
+      </article>
+      <article>
+        <ul className="list-about">
+          <li className="about-item">SERVICES</li>
+          <ul className="list-about-item">
+            <li>AI Integration</li>
+            <li>Frontend Development</li>
+            <li>Backend Development</li>
+            <li>Basic Data Analysis</li>
+          </ul>
+          <li className="about-item">RECOGNITION</li>
+          <ul className="list-about-item">
+            <li>100+ Platzi courses completed</li>
+            <li>Studying maths and software at Oviedo</li>
+          </ul>
+        </ul>
+      </article>
+    </div>
+  );
 }
 
 export default AboutMe;
+
--- src/components/AppContent.jsx
+++ src/components/AppContent.jsx
@@ -1,155 +1,173 @@
-import { useTheme } from '../context/ThemeContext';
-import MobileNavBar from './MobileNavBar';
-import DesktopNavBar from './DesktopNavBar';
-import ScrollProgressBar from './ScrollProgressBar';
-import ProjectView from './ProjectView';
-import AboutMe from './AboutMe';
-import MyFooter from './MyFooter';
-import { Typewriter } from 'react-simple-typewriter';
-import projectsData from '../data/projects.json';
-import Education from './Education';
-import AnimateOnScroll from './AnimateOnScroll';
-import Experience from './Experience';
-import { useState } from 'react';
-
+import { useTheme } from "../context/ThemeContext";
+import MobileNavBar from "./MobileNavBar";
+import DesktopNavBar from "./DesktopNavBar";
+import ScrollProgressBar from "./ScrollProgressBar";
+import ProjectView from "./ProjectView";
+import AboutMe from "./AboutMe";
+import MyFooter from "./MyFooter";
+import { Typewriter } from "react-simple-typewriter";
+import projectsData from "../data/projects.json";
+import Education from "./Education";
+import AnimateOnScroll from "./AnimateOnScroll";
+import Experience from "./Experience";
+import { useState } from "react";
 
 function AppContent() {
-    const { isDark } = useTheme();
-    const [monthsWorked, setMonthsWorked] = useState(0);
+  const { isDark } = useTheme();
+  const [monthsWorked, setMonthsWorked] = useState(0);
 
-    // Display the time worked -> format XX years, YY months
-    const formatTimeWorked = (months) => {
-        const years = Math.floor(months / 12);
-        const remainingMonths = months % 12;
-        let result = '';
-        if (years > 0) {
-            result += `${years} year${years > 1 ? 's' : ''}`;
-        }
-        if (remainingMonths > 0) {
-            if (years > 0) result += ', ';
-            result += `${remainingMonths} month${remainingMonths > 1 ? 's' : ''}`;
-        }
-        return result || '0 months';
-    };
+  // Display the time worked -> format XX years, YY months
+  const formatTimeWorked = (months) => {
+    const years = Math.floor(months / 12);
+    const remainingMonths = months % 12;
+    let result = "";
+    if (years > 0) {
+      result += `${years} year${years > 1 ? "s" : ""}`;
+    }
+    if (remainingMonths > 0) {
+      if (years > 0) result += ", ";
+      result += `${remainingMonths} month${remainingMonths > 1 ? "s" : ""}`;
+    }
+    return result || "0 months";
+  };
 
-    return (
-        <main className={`bg-(--bg-primary) transition-colors duration-300 ${isDark ? 'dark-theme' : 'light-theme'}`}>
-            <ScrollProgressBar />
-            {/* Nav responsive */}
-            <div className="md:hidden">
-                <MobileNavBar />
-            </div>
-            <div className="hidden md:block">
-                <DesktopNavBar />
-            </div>
-            
-            
-        <header className='w-9/10 mx-auto lg:max-w-[1200px]'>
+  return (
+    <main
+      className={`bg-(--bg-primary) transition-colors duration-300 ${isDark ? "dark-theme" : "light-theme"}`}
+    >
+      <ScrollProgressBar />
+      {/* Nav responsive */}
+      <div className="md:hidden">
+        <MobileNavBar />
+      </div>
+      <div className="hidden md:block">
+        <DesktopNavBar />
+      </div>
+
+      <header className="w-9/10 mx-auto lg:max-w-[1200px]">
+        <AnimateOnScroll direction="fromLeft">
+          <h2 className="title-underline text-xl text-(--text-tertiary) mt-40 ">
+            PORTFOLIO 2026
+          </h2>
+        </AnimateOnScroll>
+      </header>
+      <main className="w-9/10 mx-auto mt-12 lg:max-w-[1200px]">
+        <section className="mb-50">
+          <AnimateOnScroll direction="fromRight" delay={0.2}>
+            <h1 className="text-6xl font-bold font-mono leading-tight min-h-38 md:text-8xl lg:text-9xl text-(--text-primary)">
+              <Typewriter
+                words={["DIEGO DÍAZ MENDAÑA"]}
+                loop={0} // 0 = infinito
+                cursor
+                cursorStyle="|"
+                typeSpeed={100}
+                deleteSpeed={30}
+                delaySpeed={2000} // tiempo que espera antes de empezar a borrar
+              />
+            </h1>
+          </AnimateOnScroll>
+          <div className="info-container flex flex-col gap-10 mt-16">
+            <AnimateOnScroll direction="fromRight" delay={0.4}>
+              <article className="brief-info-container">
+                <ul>
+                  <li>
+                    <h6 className="text-xl font-medium md:text-2xl">
+                      LOCATION
+                    </h6>
+                    <p className="md:text-xl">Oviedo, España</p>
+                  </li>
+
+                  <li>
+                    <h6 className="text-xl font-medium md:text-2xl">STATUS</h6>
+                    <p className="md:text-xl">Studying & Working</p>
+                  </li>
+                  <li>
+                    <h6 className="text-xl font-medium md:text-2xl">
+                      EXPERIENCE
+                    </h6>
+                    <p className="md:text-xl">
+                      {formatTimeWorked(monthsWorked)}
+                    </p>
+                  </li>
+                </ul>
+              </article>
+            </AnimateOnScroll>
+
+            <AnimateOnScroll direction="fromLeft" delay={0.4}>
+              <article className="brief-resume-container">
+                <p className="main-text">
+                  Studying a double degree in{" "}
+                  <strong>Software Engineering</strong> and{" "}
+                  <strong>Mathematics</strong> in Oviedo. Currently working as a{" "}
+                  <strong>full-stack developer</strong> and{" "}
+                  <strong>AI integrations</strong> as a intern at the Fundación
+                  de la Universidad de Oviedo.
+                </p>
+              </article>
+            </AnimateOnScroll>
+          </div>
+        </section>
+
+        <section id="work" className="mb-80">
           <AnimateOnScroll direction="fromLeft">
-            <h2 className='title-underline text-xl text-(--text-tertiary) mt-40 '>
-              PORTFOLIO 2025
+            <h2 className="text-5xl font-bold mb-8 lg:text-6xl text-(--text-primary)">
+              SELECTED WORK
             </h2>
           </AnimateOnScroll>
-        </header>
-        <main className='w-9/10 mx-auto mt-12 lg:max-w-[1200px]'>
-          <section className='mb-50'>
-            <AnimateOnScroll direction="fromRight" delay={0.2}>
-              <h1 className='text-6xl font-bold font-mono leading-tight min-h-38 md:text-8xl lg:text-9xl text-(--text-primary)'>
-                <Typewriter
-                  words={['DIEGO DÍAZ MENDAÑA']}
-                  loop={0} // 0 = infinito
-                  cursor
-                  cursorStyle='|'
-                  typeSpeed={100}
-                  deleteSpeed={30}
-                  delaySpeed={2000} // tiempo que espera antes de empezar a borrar
+
+          <AnimateOnScroll direction="fromRight" delay={0.2}>
+            <p className="main-text lg:w-7/10">
+              A collection of some of my projects in both web development and
+              AI.
+            </p>
+          </AnimateOnScroll>
+
+          <div>
+            {projectsData.projects.map((project, index) => (
+              <AnimateOnScroll
+                key={project.id || index}
+                direction={index % 2 === 0 ? "fromLeft" : "fromRight"}
+                delay={0.1 * index}
+              >
+                <ProjectView
+                  id={index + 1}
+                  title={project.name}
+                  description={project.description}
+                  status={project.status || "down"}
+                  image={project.image}
+                  technologies={project.technologies || []}
+                  year={project.year}
+                  genre={project.genre}
+                  link={project.link}
                 />
-              </h1>
-            </AnimateOnScroll>
-            <div className='info-container flex flex-col gap-10 mt-16'>
-              <AnimateOnScroll direction="fromRight" delay={0.4}>
-                <article className='brief-info-container'>
-                  <ul>
-                    <li>
-                      <h6 className='text-xl font-medium md:text-2xl'>LOCATION</h6>
-                      <p className='md:text-xl'>Oviedo, España</p>
-                    </li>
-
-                    <li>
-                      <h6 className='text-xl font-medium md:text-2xl'>STATUS</h6>
-                      <p className='md:text-xl'>Studying. Available for Projects</p>
-                    </li>
-                    <li>
-                      <h6 className='text-xl font-medium md:text-2xl'>EXPERIENCE</h6>
-                      <p className='md:text-xl'>{formatTimeWorked(monthsWorked)}</p>
-                    </li>
-                  </ul>
-                </article>
               </AnimateOnScroll>
+            ))}
+          </div>
+        </section>
 
-              <AnimateOnScroll direction="fromLeft" delay={0.4}>
-                <article className='brief-resume-container'>
-                  <p className='main-text'>Studying a double degree in <strong>Software Engineering</strong> and <strong>Mathematics</strong> in Oviedo. Currently a student at Platzi and I am interested in <strong>full-stack</strong> development, <strong>Artificial Intelligence</strong> and Machine Learning.</p>
-                </article>
-              </AnimateOnScroll>
-            </div>
-          </section>
+        <section id="experience" className="mb-80">
+          <AnimateOnScroll direction="fromLeft">
+            <Experience setMonthsWorked={setMonthsWorked} />
+          </AnimateOnScroll>
+        </section>
 
-          <section id='work' className='mb-80'>
-            <AnimateOnScroll direction="fromLeft">
-              <h2 className='text-5xl font-bold mb-8 lg:text-6xl text-(--text-primary)'>SELECTED WORK</h2>
-            </AnimateOnScroll>
+        <section id="education" className="mb-80">
+          <AnimateOnScroll direction="fromLeft">
+            <Education />
+          </AnimateOnScroll>
+        </section>
 
-            <AnimateOnScroll direction="fromRight" delay={0.2}>
-              <p className='main-text lg:w-7/10'>A collection of some of my projects in both web development and AI.</p>
-            </AnimateOnScroll>
-
-            <div>
-              {projectsData.projects.map((project, index) => (
-                <AnimateOnScroll 
-                  key={project.id || index}
-                  direction={index % 2 === 0 ? "fromLeft" : "fromRight"}
-                  delay={0.1 * index}
-                >
-                  <ProjectView 
-                    id={index + 1}
-                    title={project.name}
-                    description={project.description}
-                    status={project.status || 'down'}
-                    image={project.image}
-                    technologies={project.technologies || []}
-                    year={project.year}
-                    genre={project.genre}
-                    link={project.link}
-                  />
-                </AnimateOnScroll>
-              ))}
-            </div>
-          </section>
-
-          <section id='experience' className='mb-80'>
-            <AnimateOnScroll direction="fromLeft">
-              <Experience setMonthsWorked={setMonthsWorked} />
-            </AnimateOnScroll>
-          </section>
-
-          <section id='education' className='mb-80'>
-            <AnimateOnScroll direction="fromLeft">
-              <Education />
-            </AnimateOnScroll>
-          </section>
-
-          <section id='about' className='mb-80'>
-            <AnimateOnScroll direction="fromLeft">
-              <AboutMe />
-            </AnimateOnScroll>
-          </section>
-        </main>
-        <footer id='contact'>
-          <MyFooter />
-        </footer>
+        <section id="about" className="mb-80">
+          <AnimateOnScroll direction="fromLeft">
+            <AboutMe />
+          </AnimateOnScroll>
+        </section>
       </main>
-    );
+      <footer id="contact">
+        <MyFooter />
+      </footer>
+    </main>
+  );
 }
 
 export default AppContent;
